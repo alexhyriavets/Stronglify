0ea4706cea5283438886d70ea806b8f4a0ae1e28
Alexander Hyriavets <alexander.hyriavets@adraba.io>
2019-02-17 21:32:17 +0200

    change colors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
 
-const Header = () => <div className="main-header container">Header App</div>;
+const Header = () => <div className="main-header container">Stronglify</div>;
 
 export default Header;
