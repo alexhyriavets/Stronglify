39d4180c9b2d887fded01cbf437d74a081ca06c4
Alexander Hyriavets <alexander.hyriavets@adraba.io>
2019-02-17 21:12:07 +0200

    remove build from igonre

---
+++ build/precache-manifest.9a87ca3ecaf865c910bd8207b8c59c13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "15be5b094002b2cdcd20",
+    "url": "/static/js/main.15be5b09.chunk.js"
+  },
+  {
+    "revision": "cdc7ce6ada0a58f634cf",
+    "url": "/static/js/2.cdc7ce6a.chunk.js"
+  },
+  {
+    "revision": "15be5b094002b2cdcd20",
+    "url": "/static/css/main.b2a66a13.chunk.css"
+  },
+  {
+    "revision": "cdc7ce6ada0a58f634cf",
+    "url": "/static/css/2.7a11bf6e.chunk.css"
+  },
+  {
+    "revision": "4eadcb9b363b3e9b5296e64725a55b4f",
+    "url": "/index.html"
+  }
+];
